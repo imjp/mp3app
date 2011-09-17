e751370eb48b94809d9938691296efcbc0656b1a
Jean-Paul Bernadina <jpbernadina@gmail.com>
2011-09-17 03:37:32 -0430

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3::Application.config.secret_token = 'd7b004c91f4a2a9ef4fbf5a83fe75a85ccd93dd97dbeba4d960df18c68df75fbd846d173edb8ee5899d0b96e027b90b33564d9b87f6b44e5246f48587228192a'
